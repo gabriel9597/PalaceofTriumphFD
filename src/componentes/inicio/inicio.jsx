c2a73a3f1b82f6e203080f7ed715779cd6827f4e
gabriel9597 <87389892+gabriel9597@users.noreply.github.com>
2025-02-25 14:19:07 -0300

    At Stories 11 inicio.jsx

--- src/componentes/inicio/inicio.jsx
+++ src/componentes/inicio/inicio.jsx
@@ -263,7 +263,7 @@ function Inicio() {
               </div>
             </div>
 
-            <div className="col-lg-6">
+            <div className="col-lg-6" id="rced-to-sts">
               <h1 className="display-5 fw-bold py-1">
                 Resultados
               </h1>
